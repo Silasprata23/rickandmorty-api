fcbf2d40600e951b65124aaf57d567596c8a7e61
Silasprata23 <silasprata23@gmail.com>
2026-04-14 15:16:10 -0300

    The Rick and Morty API project has been completed.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import {Header} from "./components/Header/index.jsx";
 import { Main } from "./components/Main/index.jsx";
 import { SectionCharacters } from "./components/SectionCharacters/index.jsx";
+import {Footer} from "./components/Footer/index.jsx";
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
           <Header/>
           <Main/>
           <SectionCharacters/>
+          <Footer/>
       </React.Fragment>
   )
 }
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,4 +1,13 @@
+import './style.css'
 
-export const Footer () =>{
-    
+export const Footer = () => {
+    return (
+        <div className="footer">
+            <p>
+                Desenvolvido por <span>Silas Prata Pereira</span>
+
+            </p>
+        </div>
+    )
 }
+
